909130980863f005c56fedd50ec9359e9252733e
Roshan Kharel <kharelroshan128@gmail.com>
2023-12-29 16:18:25 +0530

    Basic front-end setup

---
+++ src/frontend/script.js
@@ -0,0 +1,4 @@
+document.getElementById('connectWallet').addEventListener('click', function() {
+  // Code to connect to Ethereum wallet goes here
+  console.log('Wallet connection code goes here');
+});
