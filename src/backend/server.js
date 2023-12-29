00c24252f383286f9a493902fdc1be8678c00b1c
Roshan Kharel <kharelroshan128@gmail.com>
2023-12-29 16:26:23 +0530

    Set up basic back-end

---
+++ src/backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Ethereum Wallet Tracker Backend Running'));
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Test route is working!' });
+});
+
+app.use(express.json());
